a7cd01e7f24cc287b40fe7d04a7ca5e5f60a9be6
ayushmaan32 <ayushmaanroy.12@gmail.com>
2024-08-24 19:20:48 +0530

    Auth0 setup done,user can now login using Auth0

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const MainNav: React.FC = () => {
+  const { loginWithRedirect } = useAuth0();
   return (
     <Button
       variant="outline"
       className="hover:text-white hover:bg-yellow-500 text-yellow-500 font-bold"
+      onClick={async () => await loginWithRedirect()}
     >
       Log in
     </Button>
